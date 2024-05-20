6bf3f5e67dd2621ad445064083faba4bbd07b99f
RafaelRFAndrade <110788109+RafaelRFAndrade@users.noreply.github.com>
2024-05-20 19:52:31 -0300

    fix: Algumas dependências esquecidas

--- CMMTS.Web/Program.cs
+++ CMMTS.Web/Program.cs
@@ -13,9 +13,14 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<Connection>(new Connection(configuration));
 
 builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+builder.Services.AddScoped<ICentroDistribuicaoRepository, CentroDistribuicaoRepository>();
+builder.Services.AddScoped<IRotasRepository, RotasRepository>();
+builder.Services.AddScoped<IWaypointRepository, WaypointRepository>();
 
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
-
+builder.Services.AddScoped<ICentroDistribuicaoService, CentroDistribuicaoService>();
+builder.Services.AddScoped<IRotasService, RotasService>();
+builder.Services.AddScoped<IWaypointService, WaypointService>();
 
 var app = builder.Build();
 
